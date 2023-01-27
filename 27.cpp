564f9bce12db34a7905e61c1d90c0e7e85c182d0
Alankriti Kalsi <105043695+AlankritiKalsi-23@users.noreply.github.com>
2023-01-27 22:20:15 +0530

    Update 27.cpp

--- 27.cpp
+++ 27.cpp
@@ -1,16 +1,30 @@
+/*
+   1
+  23
+ 456
+78910
+*/
+
 #include <iostream>
 using namespace std;
 
 int main() {
   int n;
   cin>>n;
-  int i=1;
+  int i=1, count=1;
   while(i<=n)
     {
-      int j=i;
-      while(j<i*2)
+      int space=n-i;
+      while(space){
+        cout<<" ";
+        space=space-1;
+      }
+      
+      int j=1;
+      while(j<=i)
         {
-          cout<<j;
+          cout<<count;
+          count=count+1;
           j=j+1;
         }
       cout<<endl;
