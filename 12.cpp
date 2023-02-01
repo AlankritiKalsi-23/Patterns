a84c5e9858451de28f75e115ea7d40d3f5a59aa0
Alankriti Kalsi <105043695+AlankritiKalsi-23@users.noreply.github.com>
2023-02-01 20:54:55 +0530

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,25 @@
+/*
+AAAA
+BBBB
+CCCC
+DDDD
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int i = 1;
+  while (i <= n) {
+    int j = 1;
+    char ch = 'A' + i - 1;
+    while (j <= n) {
+      cout << ch;
+      j = j + 1;
+    }
+    cout << endl;
+    i = i + 1;
+  }
+}
